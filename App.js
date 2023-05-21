b8179f7869a540ba2e4e02a6eb20e67103043db9
Itzel <104706728+Itzelmariana@users.noreply.github.com>
2023-05-20 23:47:13 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import Header from './src/component/Header.js';
 import Search from './src/component/Search.js';
 import CategoryItems from './src/component/CategoryItems.js';
 
 export default function App() {
+  const [term, setTerm] = useState('Burger');
+  const categories = [
+    {
+      name: 'Burger',
+      imgUrl: require('./src/images/burger.png'),
+    },
+    {
+      name: 'Pizza',
+      imgUrl: require('./src/images/pizza.png'),
+    },
+    {
+      name: 'Pasta',
+      imgUrl: require('./src/images/pasta.png'),
+    },
+    {
+      name: 'Steak',
+      imgUrl: require('./src/images/steak.png'),
+    },
+    {
+      name: 'Cake',
+      imgUrl: require('./src/images/cake.png'),
+    },
+    {
+      name: 'Drinks',
+      imgUrl: require('./src/images/smoothies.png'),
+    },
+  ];
   return (
     <View style={styles.container}>
       <Header />
       <Search />
-      <CategoryItems />
+
+      <FlatList
+        data={categories}
+        renderItem={({ item }) => {
+          return (
+            <CategoryItems
+              name={item.name}
+              imgUrl={item.imgUrl}
+              index={index}
+              active={item.name === term}
+              onPress={() => {}}
+            />
+          );
+        }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(category) => category.name}
+      />
+
       <StatusBar style='auto' />
     </View>
   );
