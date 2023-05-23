b520dfe361fd99e4f38ef823af39a55b2853564a
Itzel <104706728+Itzelmariana@users.noreply.github.com>
2023-05-23 15:41:43 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,59 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/component/Header.js';
 import Search from './src/component/Search.js';
-import CategoryItems from './src/component/CategoryItems.js';
+
+import Categories from './src/component/Categories.js';
 
 export default function App() {
   const [term, setTerm] = useState('Burger');
-  const categories = [
-    {
-      name: 'Burger',
-      imgUrl: require('./src/images/burger.png'),
-    },
-    {
-      name: 'Pizza',
-      imgUrl: require('./src/images/pizza.png'),
-    },
-    {
-      name: 'Pasta',
-      imgUrl: require('./src/images/pasta.png'),
-    },
-    {
-      name: 'Steak',
-      imgUrl: require('./src/images/steak.png'),
-    },
-    {
-      name: 'Cake',
-      imgUrl: require('./src/images/cake.png'),
-    },
-    {
-      name: 'Drinks',
-      imgUrl: require('./src/images/smoothies.png'),
-    },
-  ];
+
   return (
     <View style={styles.container}>
       <Header />
-      <Search />
-
-      <FlatList
-        data={categories}
-        renderItem={({ item }) => {
-          return (
-            <CategoryItems
-              name={item.name}
-              imgUrl={item.imgUrl}
-              index={index}
-              active={item.name === term}
-              onPress={() => {}}
-            />
-          );
-        }}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        keyExtractor={(category) => category.name}
-      />
+      <Search setTerm={setTerm} />
+      <Categories setTerm={setTerm} term={term} />
 
       <StatusBar style='auto' />
     </View>
